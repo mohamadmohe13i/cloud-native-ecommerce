e9d6ebfe1ab8066e3b1ec590ff188969046754c0
mohamad <mohamadmohe13i@yahoo.com>
2023-05-19 17:12:28 +0430

    Add terraform configs for creating kubernetes cluster with Minikube

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context_cluster = "minikube"
+}
+
+provider "helm" {
+  kubernetes {
+    config_context_cluster = "minikube"
+    config_path    = "~/.kube/config"
+  }
+}
+
+resource "kubernetes_namespace" "minikube-namespace" {
+  metadata {
+    name = "shop-production"
+  }
+}
+
+resource "kubernetes_namespace" "monitoring-production" {
+  metadata {
+    name = "monitoring-production"
+  }
+}
+
+resource "helm_release" "prometheus" {
+  chart = "./prometheus"
+  name  = "prometheus"
+}
